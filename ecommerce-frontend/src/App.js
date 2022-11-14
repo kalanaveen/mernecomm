c3604340547851b921caf18df9660b9b0f2fef0a
naveen kala <kalanaveen55@gmail.com>
2022-11-14 15:59:53 +0530

    project start

---
+++ ecommerce-frontend/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="">
+      <header className="header">
+        <a href="/">MERNECOMM</a>
+      </header>
+      <main>hello</main>
+    </div>
+  );
+}
+
+export default App;
